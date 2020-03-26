6e8485880652110bfe6ede0765e2a3729bf15e77
Nick Hand <nicholas.adam.hand@gmail.com>
2020-03-26 00:10:01 -0400

    forgot vue config

---
+++ vue.config.js
@@ -0,0 +1,19 @@
+const webpack = require("webpack");
+
+module.exports = {
+    chainWebpack: config => {
+        config.module.rules.delete("eslint");
+    },
+    configureWebpack: {
+        externals: {
+            jquery: "jQuery",
+            $: "jQuery",
+        },
+        plugins: [
+            new webpack.ProvidePlugin({
+                $: "jquery",
+                jQuery: "jquery",
+            })
+        ],
+    },
+};
